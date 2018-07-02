0543ad68ea22545e5050a38a3c6a84f0156af796
darkdan21 <dan_j@live.co.uk>
2018-07-02 21:04:28 +0100

    add wrapper around yaml

---
+++ modules/fileyaml.py
@@ -0,0 +1,20 @@
+#!/bin/python
+import yaml
+class fileyaml:
+    def __init__(self,yamlfile):
+        self.yamlfile = yamlfile
+        file_object = open(yamlfile,"r")
+        stryaml = yaml.load(file_object.read())
+        self.yaml = stryaml 
+        file_object.close()
+
+    def getattr(self,attr):
+        return self.yaml.attr;
+
+    def setattr(self,attr,value):
+        self.yaml.attr = value
+        file_data = yaml.dump(data)
+        file_object = open(self.yamlfile,"w")
+        file_object.write(file_data)
+        file_object.close()
+
